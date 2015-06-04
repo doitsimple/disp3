1861859148fb1ae7dc3596b78cd8d291fa5e8013
zyp001a <yzhou01a@gmail.com>
2015-06-04 12:30:56 +0800

    add ide

--- arch/cs/src/html-angularjs/%impl.clients@html,angularjs%/js/%%main%main.js
+++ arch/cs/src/html-angularjs/%impl.clients@html,angularjs%/js/%%main%main.js
@@ -1,5 +1,42 @@
 ^^if(local.mainsub){$$
 ^^=mainsub$$
 ^^}else{$$
-
+var rootApp = angular.module('rootApp',[]);
+rootApp.controller("mainController", function($scope, $rootScope, req){
+^^=mainctrl$$
+})
+rootApp.factory('req', function($http){
+	var methods = {};
+	methods.get = function(url, headers, fn){
+		if(!fn) fn = headers;
+		if(!headers) headers = {};
+		var config = {
+      url: url,
+      method: "GET",
+      headers: headers
+    };
+		$http(config).then(function(result){
+			fn(null, result.data, result);
+		}, function(result){	
+			fn(result);
+		})
+	}
+	methods.post = function(url, data, headers, fn){
+		if(!fn) fn = headers;
+		if(!headers) headers = {};
+		if(!data) data = "";
+		var config = {
+      url: url,
+      method: "POST",
+			data: data,
+      headers: headers
+    };
+		$http(config).then(function(result){
+			fn(null, result.data, result);
+		}, function(result){	
+			fn(result);
+		})
+	}
+	return methods;
+});
 ^^}$$
---
+++ ide/js/%%mainctrl%main.js
@@ -0,0 +1,3 @@
+req.get("/read/test1/project.json", function(err, data, info){
+	$scope.data = data;
+})
