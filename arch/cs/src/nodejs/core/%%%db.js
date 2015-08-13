6fc2935bb5b69e2a8f707aa0eb518fe33aafff47
zyp001a <yzhou01a@gmail.com>
2015-08-13 11:30:31 +0800

    fix bug of init

--- arch/cs/src/nodejs/core/%%%db.js
+++ arch/cs/src/nodejs/core/%%%db.js
@@ -4,28 +4,32 @@ var dbnameMap = {};
 var genModelFuncList = {};
 var connectFuncs = [];
 var initFuncs = [];
-/*^^for(var db in global.impl.databases){var dbconfig=global.impl.databases[db];$$*/
+^^for(var db in global.impl.databases){var dbconfig=global.impl.databases[db];$$
 var ^^=db$$ = require("./^^=db$$")(dbnameMap, genModelFuncList, connectFuncs);
 for(var key in ^^=db$$){
 	module.exports[key] = ^^=db$$[key];
 }
-	/*^^for(var si=0; si<dbconfig.withSchemas.length;si++){var schema = global.proto.schemas[dbconfig.withSchemas[si]];$$*/
-		/*^^if(schema.init && schema.init.length){$$*/
-			initFuncs.push(function(cb){
+	^^for(var si=0; si<dbconfig.withSchemas.length;si++){var schema = global.proto.schemas[dbconfig.withSchemas[si]];$$
+		^^if(schema.init && schema.init.length){$$
+			initFuncs.push(function(env, cb){
 				getModel("^^=schema.name$$").select({}, function(err, doc){
+					console.log("done");
 					if(!err && !doc)
 						getModel("^^=schema.name$$").binsert(^^=JSON.stringify(schema.init)$$, cb);
+					else
+						cb();
 				});
 			});
-		/*^^}$$*/
-	/*^^}$$*/
-/*^^}$$*/
+		^^}$$
+	^^}$$
+^^}$$
 
 module.exports.connect = function (cb){
 	sync.doEachSeries(connectFuncs, function(err){
 		if(err) console.error("database connection failed");
 		sync.doEachSeries(initFuncs, function(err){
 			if(err) console.error("database init failed");
+			cb();
 		});
 		
 	});
