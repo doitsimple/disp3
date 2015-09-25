b062ea30ca937cad4c6c4a0d3e5a3391f19f9696
guimaodai <wyyxdgm@163.com>
2015-09-25 18:07:19 +0800

    session_supp

--- arch/scs/src/user/%impl.servers@nodejs-expressjs%/%%require%app.js
+++ arch/scs/src/user/%impl.servers@nodejs-expressjs%/%%require%app.js
@@ -1,3 +1,4 @@
 var Invite = require("../controllers/invite");
 var Sms = require("../controllers/sms");
 var Info = require("../controllers/info");
+var Session = require("../controllers/session");
