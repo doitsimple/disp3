22b5068b17eae0a91e2e2645e648785b88ec0384
dgm <wyyxdgm@163.com>
2015-09-29 15:03:36 +0800

    邀请吗

--- arch/scs/src/user/controllers/%%%invite.js
+++ arch/scs/src/user/controllers/%%%invite.js
@@ -4,16 +4,18 @@ module.exports.verify = function(invitecode, fn){
 	check(invitecode, function(err, invite){
 		if(err) return fn(err);
 		invite.invitecode = libRandom.genNum(11);
+		//console.log(invite.invitecode);
 		fn(null, invite);
 	});
 }
 function check(invitecode, fn){
 	if(!invitecode) return fn(null, {invite: "none"});
 	invitecode = invitecode.replace(/ /g,"").toLowerCase();
-	var Invite = db.getModels("invite");
-	var User = db.getModels("user");
+	var Invite = db.getModel("invite");
+	var User = db.getModel("user");
 	Invite.select({code: invitecode}, function(err, doc){
 		if(err) return fn(err);
+		console.log(doc);
 		if(!doc || !doc.name){
 			User.select({invitecode: invitecode}, function(err, user){
 				if(err) return fn(err);
