5574e99359822b27d37edcbe4c9cbbcd228ed45f
guimaodai <wyyxdgm@163.com>
2015-10-14 18:28:50 +0800

    socket app log.setLevel=3

--- arch/scs/src/socket/%impl.servers@nodejs-socket%/%%%app.js
+++ arch/scs/src/socket/%impl.servers@nodejs-socket%/%%%app.js
@@ -3,6 +3,7 @@ var db = require("../db");
 var log = require("../lib/log");
 var clients = {};
 var interServerAddrs = ^^=$.stringify(servers)$$ || {};
+log.setLevel(3);
 //用户在线补发离线消息时 data._id 存在
 function sendMsg(params) {
 	if(!params.type) params.type = 1;
