101def461a4ebf62d2d2e3673b0597571bc2993e
zyp001a <yzhou01a@gmail.com>
2015-10-19 15:36:17 +0800

    fix encrypt error

--- arch/scs/src/main/script/deploy_%impl.scripts@deploy%.bash
+++ arch/scs/src/main/script/deploy_%impl.scripts@deploy%.bash
@@ -5,4 +5,4 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 echo $DIR
 
 cd $DIR/.. && $DISP3 ^^=name$$ && cd -
-cd ^^=localurl || "../"+name$$ && rsync -avz -e "ssh -p ^^=port$$" . ^^=remoteurl$$ && ssh -p ^^=port$$ ^^=remoteurl.split(':')[0]$$ "cd ^^=remoteurl.split(':')[1] || '.'$$ && ./script/server_restart"
+cd ^^=localurl || "../"+name$$ && rsync -avz -e "ssh -p ^^=port$$" . ^^=remoteurl$$
