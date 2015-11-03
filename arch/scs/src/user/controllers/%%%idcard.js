cd799c2c44cea4545d342c9eb7ac5dcba1f2afb1
guimaodai <wyyxdgm@163.com>
2015-11-03 14:42:45 +0800

    idcard

--- arch/scs/src/user/controllers/%%%idcard.js
+++ arch/scs/src/user/controllers/%%%idcard.js
@@ -1,35 +1,39 @@
-var db =require("../db");
+var db = require("../db");
 var log = require("../lib/log");
 var defaultPlatform;
 module.exports.setPlatform = setPlatform;
-function setPlatform(platform){
+
+function setPlatform(platform) {
 	defaultPlatform = platform;
 }
 module.exports.verify = verify;
-function verify(params, fn){
+
+function verify(params, fn) {
 	var platform = params.platform || defaultPlatform;
-	if(!platform) fn("no platform!");
+	if (!platform) fn("no platform!");
 	var Model = db.getModel("idcard");
 	Model.select({
 		idcard: params.idcard,
 		realname: params.realname
-	}, function(err, result){
-		if(err) return fn(err);
-		if(result) fn(null);
+	}, function(err, result) {
+		if (err) return fn(err);
+		if (result) fn(null, {
+			success: true
+		});
 		var p;
-		try{
+		try {
 			p = require("./" + platform);
-			if(!p.verifyIdcard) return fn("not method send in platfrom");
-		}catch(e){
+			if (!p.verifyIdcard) return fn("no method verifyIdcard in platfrom");
+		} catch (e) {
 			return fn("platform " + params.platform + " is not found");
 		}
-		p.verifyIdcard(params, function(err, result){
-			if(!result) result = {};
+		p.verifyIdcard(params, function(err, result) {
+			if (!result) result = {};
 			result.idcard = params.idcard;
 			result.realname = params.realname;
-			if(err) {
+			if (err) {
 				/*^^if(!global.product){$$*/
-				if(err.code == "VERIFY_BINDED_OVERRUN" || err.code == "DUPLICATE_VERIFY"){
+				if (err.code == "VERIFY_BINDED_OVERRUN" || err.code == "DUPLICATE_VERIFY") {
 					log.i("认证超限，测试环境返回正确");
 					result.weibopayid = "jac" + params.idcard;
 					return fn(null, result);
@@ -40,8 +44,8 @@ function verify(params, fn){
 			Model.upsert({
 				idcard: params.idcard,
 				realname: params.realname
-			}, {}, function(err){
-				if(err) return fn(err);
+			}, {}, function(err) {
+				if (err) return fn(err);
 				fn(null, result);
 			});
 		});
