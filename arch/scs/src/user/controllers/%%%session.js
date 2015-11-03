500352a01d73ffd2d67bd86468193ca0f76353ed
guimaodai <wyyxdgm@163.com>
2015-11-03 16:20:09 +0800

    session ip userid

--- arch/scs/src/user/controllers/%%%session.js
+++ arch/scs/src/user/controllers/%%%session.js
@@ -1,11 +1,13 @@
 var Session = {};
 var db = require("../db");
+var log = require("../lib/log");
 Session.supplementary = function(param, fn) {
 	var session = param.session;
 	var index = param.index;
 	var device_token = param.device_token;
 	var content = param.content;
 	var userid = param.userid;
+	var ip = param.ip;
 	if (!session) return fn("no session");
 	if (!device_token) return fn("no device_token");
 	session = session.toString();
@@ -21,7 +23,7 @@ Session.supplementary = function(param, fn) {
 		});
 		var json = {};
 		if (userid && !doc.userid)
-			json.userid = parseInt(userid);
+			json.userid = userid;
 		if (!doc.lastindex) {
 			//第一次传用户体验数据
 			if (index != 1)
@@ -34,6 +36,22 @@ Session.supplementary = function(param, fn) {
 			json.lastindex = index;
 			json.content = doc['content'] + content;
 		}
+		var User = db.getModel("user");
+		if (doc.channel) {
+			User.update({
+				_id: userid,
+				channel: {
+					$exists: false
+				}
+			}, {
+				channel: doc.channel
+			}, function(err, doc) {
+				if (err) log.e("err when User.update where _id:" + userid);
+			});
+		} else {
+			log.e("no session.channel for session._id" + doc._id);
+		}
+
 		Session.update({
 			"session": session,
 			"device_token": device_token
