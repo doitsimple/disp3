7bc37f9ac29cf988611138f54592cb5412d8f524
zyp001a <yzhou01a@gmail.com>
2016-02-26 15:55:00 +0800

    db can insert now

--- arch/server/expressjs/db.js
+++ arch/server/expressjs/db.js
@@ -1,1 +1,7 @@
-
+^^for(var dbname in databases){
+var dbconfig = databases[dbname];
+if(!dbconfig.type) dbconfig.tyoe = "mongodb";
+if(!dbconfig.name) dbconfig.name = dbname;
+$$
+module.exports.^^=dbname$$ = ^^=~Database: dbconfig$$;
+^^}$$
