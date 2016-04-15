c7bea678ce25f9020f13268b4373e8b2124df717
zyp001a <yzhou01a@gmail.com>
2016-04-15 16:58:02 +0800

    mod requestx and add angular partial

--- arch/client/angularjs/main.js
+++ arch/client/angularjs/main.js
@@ -1,11 +1,11 @@
-var app = angular.module('app', []);
+var app = angular.module('app', [^^=~evalStringArray: Object.keys(global.angularDeps), sep:","$$]);
+^^=~evalDic: global.angularDeps$$
 ^^
 for(var key in global.controllers){
  var ctrl = global.controllers[key];
- 
 $$
 app.controller("^^=key$$Controller", function($scope){
-^^=~ctrl.code$$
+^^=~extend: ctrl.content, error: ctrl.error, success:ctrl.success$$
 });
 
 ^^}$$
@@ -16,7 +16,6 @@ for(var key in global.angularLib){
 $$
 ^^=~toeval$$; 
 ^^}$$
-
-
+^^=~content$$
 
 
